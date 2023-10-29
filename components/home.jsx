9696636f5e13731b5e4c523347139ea4daf7d436
Anubhavgupta14 <anubhavgu2002@gmail.com>
2023-10-29 23:00:55 +0530

    ok

--- components/home.jsx
+++ components/home.jsx
@@ -3,31 +3,9 @@ import Link from 'next/link'
 import { useEffect, useState } from 'react'
 
 const home = () => {
-  const [name, setName] = useState("");
-
-  const func = async() => {
-    const response = await fetch('/api/hello', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    let data = await response.json();
-    const { user } = data;
-    setName(data.name)
-    console.log(data.name)
-    
-  }
-  useEffect(() => {
-    func();
-  }, []);
-
   return (
     <div>
       <h2 className='heading'>Shiksha Hub</h2>
-      <Link href="/dash"><button>Click</button></Link>
-      <p>Name : {name}</p>
-
     </div>
   )
 }
